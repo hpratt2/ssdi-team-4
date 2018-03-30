fd80d827667311cc2e0be03e59c90751183b9e5b
Jacob Greene <35856498+greenej10@users.noreply.github.com>
2018-03-29 22:33:36 -0400

    Test Edit

--- src/winevault/api/WineListingControllerTest.java
+++ src/winevault/api/WineListingControllerTest.java
@@ -13,18 +13,17 @@ import winevault.model.Wine;
 import winevault.service.WineListingService;
 
 class WineListingControllerTest extends WineListingController {
-	
-	
-	List<IWine> test = new ArrayList<IWine>();
-	Wine w1 = new Wine(1,"White Blend", "Italy", 87.00, 10.00, 12.00);
-	Wine w2 = new Wine(2,"Portuguese Red", "Portugal", 87.00, 15.00, 15.00);
-	Wine w3 = new Wine(3,"Pinot Gris", "US", 87.33, 14.00, 27.00);
+
 	
 	
 	
 
 	@Test
 	void testGetWineList() {
+		List<IWine> test = new ArrayList<IWine>();
+		Wine w1 = new Wine(1,"White Blend", "Italy", 87.00, 10.00, 12.00);
+		Wine w2 = new Wine(2,"Portuguese Red", "Portugal", 87.00, 15.00, 15.00);
+		Wine w3 = new Wine(3,"Pinot Gris", "US", 87.33, 14.00, 27.00);
 		test.add(w1);
 		test.add(w2);
 		test.add(w3);
@@ -44,6 +43,10 @@ class WineListingControllerTest extends WineListingController {
 
 	@Test
 	void testGetSortedWineList() {
+		List<IWine> test = new ArrayList<IWine>();
+		Wine w1 = new Wine(1,"White Blend", "Italy", 87.00, 10.00, 12.00);
+		Wine w2 = new Wine(2,"Portuguese Red", "Portugal", 87.00, 15.00, 15.00);
+		Wine w3 = new Wine(3,"Pinot Gris", "US", 87.33, 14.00, 27.00);
 		test.add(w1);
 		test.add(w2);
 		test.add(w3);
